4332e20f80bded133047b4f158ec302f7f39b97d
Ramon Bolivar Cuevas Escobar <rcuevase@gmail.com>
2023-05-31 18:16:00 -0600

    homework javascript basico 1

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,8 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+
+   return string
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +13,41 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+
+   let _resultAdd = x + y;
+   return _resultAdd
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+
+   let _resultSubstract = x - y;
+   return _resultSubstract
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+
+   let _resultDivide = x/y;
+   return _resultDivide
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+
+   let _resultProduct = x * y;
+   return _resultProduct
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+
+   let _resultModule = x % y;
+   return _resultModule
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
