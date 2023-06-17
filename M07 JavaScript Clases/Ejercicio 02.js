2b4aac75bbb0f235b030aab0126897566303871b
Ramon Bolivar Cuevas Escobar <rcuevase@gmail.com>
2023-06-17 13:43:04 -0600

    homework javascript basico 5

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,7 +6,7 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
-
+   
    constructor(nombre, apellido, edad, domicilio) {
          this.nombre = nombre;
          this.apellido = apellido;
